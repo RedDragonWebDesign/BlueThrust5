e9dad33b2cf99916e77b0fc4f0f0a2c5cb37c8e1
RedDragonWebDesign <43397938+GeneralKenobi1@users.noreply.github.com>
2020-10-27 00:58:57 -0700

    add debug function html_var_export()

--- src/_functions.php
+++ src/_functions.php
@@ -286,6 +286,49 @@ function debug_string_backtrace() {
 	return $trace;
 }
 
+// Example usage: html_var_export($limit, '$limit');
+function html_var_export($var, $var_name = NULL)
+{
+	$output = '';
+	
+	if ( $var_name )
+	{
+		$output .= $var_name . ' = ';
+	}
+	
+	$output .= nl2br_and_nbsp(var_export($var, TRUE)) . "<br /><br />";
+	
+	echo $output;
+}
+
+function nl2br_and_nbsp($string)
+{
+	$string = nl2br($string);
+	
+	$string = nbsp($string);
+	
+	return $string;
+}
+
+function nbsp($string)
+{
+	$string = preg_replace('/\t/', '&nbsp;&nbsp;&nbsp;&nbsp;', $string);
+	
+	// replace more than 1 space in a row with &nbsp;
+	$string = preg_replace('/  /m', '&nbsp;&nbsp;', $string);
+	$string = preg_replace('/ &nbsp;/m', '&nbsp;&nbsp;', $string);
+	$string = preg_replace('/&nbsp; /m', '&nbsp;&nbsp;', $string);
+	
+	if ( $string == ' ' )
+	{
+		$string = '&nbsp;';
+	}
+	
+	// Convert 2 space tab to 4 space tab
+	$string = preg_replace('/&nbsp;&nbsp;/m', '&nbsp;&nbsp;&nbsp;&nbsp;', $string);
+	
+	return $string;
+}
 
 // Class Loaders
 
