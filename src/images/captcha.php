3e9d56d0ce38526a1f7ad093b3df46e8b39b7b68
RedDragonWebDesign <43397938+GeneralKenobi1@users.noreply.github.com>
2020-10-29 21:16:55 -0700

    add comments to captcha file

--- src/images/captcha.php
+++ src/images/captcha.php
@@ -1,4 +1,7 @@
 <?php
+
+// NOTE: This file is very hard to debug, since any PHP warning will corrupt the image, and you won't be able to see either the warnings or the images. Use step debugging to find and fix the errors.
+
 require_once("../_setup.php");
 require_once("../classes/member.php");
 require_once("../classes/basicorder.php");
@@ -25,10 +28,12 @@ if(
 	exit();	
 }
 
-
+// Do they want HTML, or the actual image?
+// HTML
 if(isset($_GET['display'])) {
 	echo "<img src='".$MAIN_ROOT."images/captcha.php?appCompID=".$_GET['appCompID']."&new=".time()."' width='440' height='90'>";
 }
+// Actual image
 else {
 	header('Content-Type: image/png');
 	
@@ -39,7 +44,7 @@ else {
 		$appCompInfo = $appComponentObj->get_info_filtered();
 	}
 	
-	
+	// I don't think you can recycle captchas. Every time you load this page, it will generate a new captcha.
 	$captchaObj = new Basic($mysqli, "app_captcha", "appcaptcha_id");
 	$filterIP = $mysqli->real_escape_string($IP_ADDRESS);
 	$mysqli->query("DELETE FROM ".$dbprefix."app_captcha WHERE appcomponent_id = '".$appCompInfo['appcomponent_id']."' AND ipaddress = '".$filterIP."'");
@@ -47,32 +52,31 @@ else {
 	// Create the image
 	$im = imagecreatetruecolor(440, 90);
 	
-	
 	// Colors
 	$black = imagecolorallocate($im, 9, 9, 9);
 	$blackAlpha = imagecolorallocatealpha($im, 9, 9, 9,25);
 	$redAlpha = imagecolorallocatealpha($im, 255, 0, 0, 15);
 	$white = imagecolorallocate($im, 255, 255, 255);
 	
+	// Fill the image with white
 	imagefilledrectangle($im, 0, 0, 439, 89, $white);
 	
-	
-	// Generate Captcha Text
-	
+	// Generate Captcha Text (all lowercase, not final version)
 	$randString = md5(uniqid("", true));
 	$randNum = rand(0,22);
+	$captcha = substr($randString.$randString, $randNum, 8);
 	
 	$arrABC = range("a", "z");
 	shuffle($arrABC);
-	$captcha = substr($randString.$randString, $randNum, 8);
 	
 	$arrCaptcha = str_split($captcha);
 	$finalCaptchaText = "";
 	
-	$distortFont = "captcha-fonts/RFX_Splatz.ttf";
+	$distortFont = $BASE_DIRECTORY."captcha-fonts/RFX_Splatz.ttf";
 	$arrDistort = range("a", "i");
 	$counter = 0;
 	
+	// foreach letter
 	foreach($arrCaptcha as $value) {
 		// Font Locations
 		$arrFonts = array();
@@ -86,7 +90,7 @@ else {
 		if(is_numeric($value) && rand(1, 10) > 2) {
 			$char = (rand(1,10) > 5) ? strtoupper($arrABC[$value]) : $value;
 		}
-		elseif(!is_numeric($value) && rand(1, 10) > 5) {
+		elseif(!is_numeric($value) && rand(1, 10) > 5) {	// randomly uppercase some of the letters
 			$char = strtoupper($value);
 		}
 		
@@ -108,16 +112,13 @@ else {
 			unset($arrFonts[3]);
 		}
 		
+		// pick a random font
 		shuffle($arrFonts);
-		
-		
 		$randFontNum = rand(0,(count($arrFonts)-1));
 		$randFont = $arrFonts[$randFontNum];
-		
 		$randNum2 = rand(0,8);
 		
-		
-		
+		// if component type is "captchaextra", add lots of splats and noise using the special font RFX Splatz
 		if($appComponentObj->get_info("componenttype") == "captchaextra") {
 			$xCoord = ($counter == 0) ? 10 : (55*$counter);
 			imagettftext($im, 70, rand(0,20), $xCoord, 80, $blackAlpha, $distortFont, $arrDistort[$randNum2]);
@@ -125,17 +126,25 @@ else {
 		
 		$xCoord = ($counter == 0) ? 10 : (55*$counter);
 		
+		// place this character in the image
 		imagettftext($im, 50, rand(-5,5), $xCoord, 70, $black, $randFont, $char);
 		$counter++;
+		
+		// note the final captcha (different than original, some letters randomly capitalized)
 		$finalCaptchaText .= $char;
 	}
 	
+	// log the final captcha in the database
 	$captchaObj->addNew(
 		array("appcomponent_id", "ipaddress", "captchatext"),
 		array($appCompInfo['appcomponent_id'],
 		$IP_ADDRESS,
 		strtolower($finalCaptchaText))
 	);
+	
+	// output the image
 	imagepng($im);
+	
+	// free the memory
 	imagedestroy($im);
 }
