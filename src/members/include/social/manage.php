b8c3a738bfc64ef59668ca708ed1613339170e47
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2024-01-20 23:19:31 -0700

    Update manage.php
    error reduction mode.

--- src/members/include/social/manage.php
+++ src/members/include/social/manage.php
@@ -12,35 +12,32 @@
  *
  */
 
-
 if(!isset($member) || substr($_SERVER['PHP_SELF'], -11) != "console.php") {
 	exit();
 }
 else {	
 	$memberInfo = $member->get_info_filtered();
-	$consoleObj->select($_GET['cID']);
+	$consoleObj->select($_GET['cID'] ?? '');
 	if(!$member->hasAccess($consoleObj)) {
 		exit();
 	}
-	
 }
 
-$cID = $_GET['cID'];
+$cID = $_GET['cID'] ?? '';
 
 $socialObj = new Social($mysqli);
 $objManageList = new btOrderManageList($socialObj);
 $objManageList->strMainListLink = BASE_DIRECTORY."members/include/social/include/main.php";
 
-if($_GET['sID'] != "" && $socialObj->select($_GET['sID']) && $_GET['action'] == "edit") {
+// Check if 'sID' and 'action' are set in the $_GET array
+if(isset($_GET['sID'], $_GET['action']) && $_GET['sID'] != "" && $socialObj->select($_GET['sID']) && $_GET['action'] == "edit") {
 	require_once("include/edit.php");
 }
-elseif($_GET['action'] == "delete" && $socialObj->select($_POST['itemID'])) {
+elseif(isset($_GET['action'], $_POST['itemID']) && $_GET['action'] == "delete" && $socialObj->select($_POST['itemID'])) {
 	$socialInfo = $socialObj->get_info_filtered();
 	$objManageList->strDeleteName = $socialInfo['name'];
 	$objManageList->strDeletePostVarID = "sID";
-	
-	
 }
-elseif($_GET['action'] != "move") {	
+elseif(!isset($_GET['action']) || $_GET['action'] != "move") {	
 	require_once($objManageList->strMainListLink);
 }
