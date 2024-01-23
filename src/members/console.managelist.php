b84d32872b9ca0f1a68804a1de035c9bcd448c23
deepend <deepend@tilde.club>
2024-01-23 01:35:38 -0700

    fix console managelist

--- src/members/console.managelist.php
+++ src/members/console.managelist.php
@@ -1,112 +1,103 @@
 <?php
 
-	/*
-	 * BlueThrust Clan Scripts
-	 * Copyright 2014
-	 *
-	 * Author: Bluethrust Web Development
-	 * E-mail: support@bluethrust.com
-	 * Website: http://www.bluethrust.com
-	 *
-	 * License: http://www.bluethrust.com/license.php
-	 *
-	 */
+/*
+ * BlueThrust Clan Scripts
+ * Copyright 2014
+ *
+ * Author: Bluethrust Web Development
+ * E-mail: support@bluethrust.com
+ * Website: http://www.bluethrust.com
+ *
+ * License: http://www.bluethrust.com/license.php
+ *
+ */
 
-	if(!defined("LOGGED_IN") || !LOGGED_IN) { die("<script type='text/javascript'>window.location = '".$MAIN_ROOT."'</script>"); }
-	
-	
-	$actionsWidth = count($setupManageListArgs['actions'])*6;
-	$titleWidth = 100-($actionsWidth);
-	
-	
+	if (!defined("LOGGED_IN") || !LOGGED_IN) {
+		die("<script type='text/javascript'>window.location = '" . $MAIN_ROOT . "'</script>");
+	}
+
+
+	$actionsWidth = count($setupManageListArgs['actions']) * 6;
+	$titleWidth = 100 - ($actionsWidth);
+
+
 	// Setup default values if not given
-	$actionsTitleName = ($setupManageListArgs['action_title'] == "") ? "Actions:" : $setupManageListArgs['action_title'];
-	$itemTitleName = ($setupManageListArgs['item_title'] == "") ? "Item:" : $setupManageListArgs['item_title'];
-	
-	$dispAddNewLink = (!isset($setupManageListArgs['add_new_link']['url']) || $setupManageListArgs['add_new_link']['url'] == "") ? "" : "&raquo; <a href='".$setupManageListArgs['add_new_link']['url']."'>".$setupManageListArgs['add_new_link']['name']."</a> &laquo;";
-	
-	$setupManageListArgs['list_div_name'] = ($setupManageListArgs['list_div_name'] == "") ? "manageListDiv" : $setupManageListArgs['list_div_name'];
-	
-	$setupManageListArgs['loading_spiral'] = ($setupManageListArgs['loading_spiral'] == "") ? "manageListLoadingSpiral" : $setupManageListArgs['loading_spiral'];
-	
-	
-	
-	
+	$actionsTitleName = $setupManageListArgs['action_title'] ?? "Actions:";
+	$itemTitleName = $setupManageListArgs['item_title'] ?? "Item:";
+
+	$dispAddNewLink = (!isset($setupManageListArgs['add_new_link']['url']) || $setupManageListArgs['add_new_link']['url'] == "") ? "" : "&raquo; <a href='" . $setupManageListArgs['add_new_link']['url'] . "'>" . $setupManageListArgs['add_new_link']['name'] . "</a> &laquo;";
+
+	$setupManageListArgs['list_div_name'] = $setupManageListArgs['list_div_name'] ?? "manageListDiv";
+	$setupManageListArgs['loading_spiral'] = $setupManageListArgs['loading_spiral'] ?? "manageListLoadingSpiral";
+
+
 	// Display Manage List
-	
+
 	echo "
 
 		<table class='formTable'>
 			<tr>
-				<td colspan='2' align='right'>".$dispAddNewLink."<br><br></td>
+				<td colspan='2' align='right'>" . $dispAddNewLink . "<br><br></td>
 			</tr>
 			<tr>
-				<td class='formTitle' style='width: ".$titleWidth."%'>".$itemTitleName."</td>
-				<td class='formTitle' style='width: ".$actionsWidth."%'>".$actionsTitleName."</td>
+				<td class='formTitle' style='width: " . $titleWidth . "%'>" . $itemTitleName . "</td>
+				<td class='formTitle' style='width: " . $actionsWidth . "%'>" . $actionsTitleName . "</td>
 			</tr>
 		</table>
 		
-		<div class='loadingSpiral' id='".$setupManageListArgs['loading_spiral']."'><p align='center'><img src='".$MAIN_ROOT."themes/".$THEME."/images/loading-spiral.gif'><br>Loading...</p></div>
+		<div class='loadingSpiral' id='" . $setupManageListArgs['loading_spiral'] . "'><p align='center'><img src='" . $MAIN_ROOT . "themes/" . $THEME . "/images/loading-spiral.gif'><br>Loading...</p></div>
 		
-		<div id='".$setupManageListArgs['list_div_name']."'>
+		<div id='" . $setupManageListArgs['list_div_name'] . "'>
 	";
-		
-		require_once("console.managelist.list.php");
-		
+
+	require_once("console.managelist.list.php");
+	$moveLink = $setupManageListArgs['move_link'] ?? "defaultMoveLink.php"; // Replace 'defaultMoveLink.php' with your default link
 	echo "</div>
 	
 	
 		<div id='confirmDeleteDialog'></div>
 		<script type='text/javascript'>
 		
-			function moveItem(move_dir, item_id) {
-		
-				$(document).ready(function() {
-				
-					$('#".$setupManageListArgs['loading_spiral']."').show();
-					$('#".$setupManageListArgs['list_div_name']."').fadeOut(250);
-					//".$setupManageListArgs['move_link']."
-					$.post('".$MAIN_ROOT."members/console.managelist.move.php?cID=".filterText($_GET['cID'])."', { itemID: item_id, moveDir: move_dir }, function(data) {
-					
-						$('#".$setupManageListArgs['loading_spiral']."').hide();
-						$('#".$setupManageListArgs['list_div_name']."').html(data).fadeIn(250);
-						
-					});
-					
-				
-				});
-			
-			}
+    function moveItem(move_dir, item_id) {
+        $(document).ready(function() {
+            $('#" . $setupManageListArgs['loading_spiral'] . "').show();
+            $('#" . $setupManageListArgs['list_div_name'] . "').fadeOut(250);
+            $.post('" . $moveLink . "', { itemID: item_id, moveDir: move_dir }, function(data) {
+                $('#" . $setupManageListArgs['loading_spiral'] . "').hide();
+                $('#" . $setupManageListArgs['list_div_name'] . "').html(data).fadeIn(250);
+            });
+        });
+    }
 			
 			function deleteItem(item_id) {
 			
 				$(document).ready(function() {
 				
 			";
-	
-			if(!$setupManageListArgs['confirm_delete']) {
 
-				echo "
-					$('#".$setupManageListArgs['loading_spiral']."').show();
-					$('#".$setupManageListArgs['list_div_name']."').fadeOut(250);
+	if (!$setupManageListArgs['confirm_delete']) {
+
+		echo "
+					$('#" . $setupManageListArgs['loading_spiral'] . "').show();
+					$('#" . $setupManageListArgs['list_div_name'] . "').fadeOut(250);
 				";
-				
-			}
-			
-	
-			echo "
+
+	}
+
+
+	echo "
 					
-					$.post('".$setupManageListArgs['delete_link']."', { itemID: item_id }, function(data) {
+					$.post('" . $setupManageListArgs['delete_link'] . "', { itemID: item_id }, function(data) {
 					
 					";
 
-					if($setupManageListArgs['confirm_delete']) {
-						
-						echo "
+	if ($setupManageListArgs['confirm_delete']) {
+
+		echo "
 							$('#confirmDeleteDialog').html(data);
 							$('#confirmDeleteDialog').dialog({
 								
-								title: '".$consoleInfo['pagetitle']." - Delete',
+								title: '" . $consoleInfo['pagetitle'] . " - Delete',
 								width: 400,
 								modal: true,
 								zIndex: 9999,
@@ -115,13 +106,13 @@
 								buttons: {
 									'Yes': function() {
 										
-										$('#".$setupManageListArgs['loading_spiral']."').show();
-										$('#".$setupManageListArgs['list_div_name']."').fadeOut(250);
+										$('#" . $setupManageListArgs['loading_spiral'] . "').show();
+										$('#" . $setupManageListArgs['list_div_name'] . "').fadeOut(250);
 										$(this).dialog('close');
 										
-										$.post('".$setupManageListArgs['delete_link']."', { itemID: item_id, confirm: 1 }, function(data1) {
-											$('#".$setupManageListArgs['loading_spiral']."').hide();
-											$('#".$setupManageListArgs['list_div_name']."').html(data1).fadeIn(250);
+										$.post('" . $setupManageListArgs['delete_link'] . "', { itemID: item_id, confirm: 1 }, function(data1) {
+											$('#" . $setupManageListArgs['loading_spiral'] . "').hide();
+											$('#" . $setupManageListArgs['list_div_name'] . "').html(data1).fadeIn(250);
 										});
 									
 									},
@@ -134,16 +125,15 @@
 								
 							});
 						";
-						
-					}
-					else {
-						echo "
-							$('#".$setupManageListArgs['loading_spiral']."').hide();
-							$('#".$setupManageListArgs['list_div_name']."').html(data).fadeIn(250);
+
+	} else {
+		echo "
+							$('#" . $setupManageListArgs['loading_spiral'] . "').hide();
+							$('#" . $setupManageListArgs['list_div_name'] . "').html(data).fadeIn(250);
 						";
-					}
-						
-				echo "	
+	}
+
+	echo "	
 					});
 					
 				
