85f879f49d9f30bedc0df399623b6664af02c171
deepend <deepend@tilde.club>
2024-01-23 01:23:37 -0700

    fix worldclocks

--- src/members/include/worldclocks/manageclocks.php
+++ src/members/include/worldclocks/manageclocks.php
@@ -13,33 +13,29 @@
 	 */
 
 
-
-	if(!isset($member) || substr($_SERVER['PHP_SELF'], -11) != "console.php") {
+	if (!isset($member) || substr($_SERVER['PHP_SELF'], -11) != "console.php") {
 		exit();
-	}
-	else {	
+	} else {
 		$memberInfo = $member->get_info_filtered();
 		$consoleObj->select($_GET['cID']);
-		if(!$member->hasAccess($consoleObj)) {
+		if (!$member->hasAccess($consoleObj)) {
 			exit();
 		}
-		
-	}
-	
-	
-	$objManageList = new btOrderManageList($clockObj);
-	$objManageList->strMainListLink = BASE_DIRECTORY."members/include/worldclocks/main.php";
 
-	
-	if($_GET['clockID'] != "" && $clockObj->select($_GET['clockID']) && $_GET['action'] == "edit") {
-		$clockInfo = $clockObj->get_info_filtered();
-		require_once(BASE_DIRECTORY."members/include/worldclocks/edit.php");
 	}
-	elseif($_GET['action'] == "delete" && $clockObj->select($_POST['itemID'])) {
+
+
+	$objManageList = new btOrderManageList($clockObj);
+	$objManageList->strMainListLink = BASE_DIRECTORY . "members/include/worldclocks/main.php";
+
+
+	if (isset($_GET['clockID']) && $_GET['clockID'] != "" && $clockObj->select($_GET['clockID']) && isset($_GET['action']) && $_GET['action'] == "edit") {
+		$clockInfo = $clockObj->get_info_filtered();
+		require_once(BASE_DIRECTORY . "members/include/worldclocks/edit.php");
+	} elseif (isset($_GET['action']) && $_GET['action'] == "delete" && isset($_POST['itemID']) && $clockObj->select($_POST['itemID'])) {
 		$info = $clockObj->get_info_filtered();
 		$objManageList->strDeleteName = $info['name'];
-		$objManageList->strDeletePostVarID = "clockID";	
-	}
-	elseif($_GET['action'] != "move") {
-		require_once($objManageList->strMainListLink);	
+		$objManageList->strDeletePostVarID = "clockID";
+	} elseif (!isset($_GET['action']) || $_GET['action'] != "move") {
+		require_once($objManageList->strMainListLink);
 	}
