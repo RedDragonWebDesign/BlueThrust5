7c22565e62558df044095fccb8ad2187503095ae
deepend <deepend@tilde.club>
2024-01-23 01:36:28 -0700

    fix PHP 8.1 depreciations/notices/warnings in src/classes

--- src/classes/rank.php
+++ src/classes/rank.php
@@ -1,196 +1,204 @@
 <?php
 
 /*
- * BlueThrust Clan Scripts
- * Copyright 2014
+* BlueThrust Clan Scripts
+* Copyright 2014
+*
+* Author: Bluethrust Web Development
+* E-mail: support@bluethrust.com
+* Website: http://www.bluethrust.com
+*
+* License: http://www.bluethrust.com/license.php
+*
+*/
+
+
+	require_once("basicorder.php");
+
+	class Rank extends BasicOrder
+	{
+
+
+		function __construct($sqlConnection)
+		{
+			$this->MySQL = $sqlConnection;
+			$this->strTableName = $this->MySQL->get_tablePrefix() . "ranks";
+			$this->strTableKey = "rank_id";
+			$this->strAssociateTableName = $this->MySQL->get_tablePrefix() . "members";
+			$this->strAssociateKeyName = "member_id";
+		}
+
+
+		function get_privileges()
+		{
+
+			$arrPrivileges = array();
+
+			if ($this->intTableKeyValue == 1) {
+				$result = $this->MySQL->query("SELECT console_id FROM " . $this->MySQL->get_tablePrefix() . "console ORDER BY sortnum");
+			} else {
+				$result = $this->MySQL->query("SELECT rp.console_id FROM " . $this->MySQL->get_tablePrefix() . "rank_privileges rp, " . $this->MySQL->get_tablePrefix() . "console c WHERE rank_id = '" . $this->intTableKeyValue . "' AND c.console_id = rp.console_id ORDER BY c.sortnum");
+			}
+
+
+			while ($row = $result->fetch_assoc()) {
+				$arrPrivileges[] = $row['console_id'];
+			}
+
+
+			return $arrPrivileges;
+		}
+
+
+/**
+ * - countMembers Function -
  *
- * Author: Bluethrust Web Development
- * E-mail: support@bluethrust.com
- * Website: http://www.bluethrust.com
  *
- * License: http://www.bluethrust.com/license.php
+ * Returns the number of members with the current selected rank
+ */
+		function countMembers()
+		{
+			$result = $this->MySQL->query("SELECT * FROM " . $this->MySQL->get_tablePrefix() . "members WHERE " . $this->strTableKey . " = '" . $this->intTableKeyValue . "'");
+			$num_rows = $result->num_rows;
+
+
+			return $num_rows;
+		}
+
+/**
+ * - getLocalImageURL Function -
+ *
+ * Used to determine if the image attached to the selected rank is a local image or external image.
+ *
+ * Returns FALSE when the image is an external url.
+ * Returns the local image address when the image is on the server.
  *
  */
+		function getLocalImageURL()
+		{
+			global $MAIN_ROOT;
+			$returnVal = false;
+			if ($this->intTableKeyValue != "") {
 
+				if (strpos($this->arrObjInfo['imageurl'], "http://") === false) {
 
-require_once("basicorder.php");
+					$returnVal = $this->arrObjInfo['imageurl'];
 
-class Rank extends BasicOrder {
-	
-	
-	
-	function __construct($sqlConnection) {
-		$this->MySQL = $sqlConnection;
-		$this->strTableName = $this->MySQL->get_tablePrefix()."ranks";
-		$this->strTableKey = "rank_id";
-		$this->strAssociateTableName = $this->MySQL->get_tablePrefix()."members";
-		$this->strAssociateKeyName = "member_id";
-	}
+				}
 
-	
-	function get_privileges() {
-	
-		$arrPrivileges = array();
-		
-		if($this->intTableKeyValue == 1) {
-			$result = $this->MySQL->query("SELECT console_id FROM ".$this->MySQL->get_tablePrefix()."console ORDER BY sortnum");
-		}
-		else {
-			$result = $this->MySQL->query("SELECT rp.console_id FROM ".$this->MySQL->get_tablePrefix()."rank_privileges rp, ".$this->MySQL->get_tablePrefix()."console c WHERE rank_id = '".$this->intTableKeyValue."' AND c.console_id = rp.console_id ORDER BY c.sortnum");
-		}
-
-		
-		
-		while($row = $result->fetch_assoc()) {
-			$arrPrivileges[] = $row['console_id'];
-		}
-	
-	
-		return $arrPrivileges;
-	}
-	
-	
-	/**
-	 * - countMembers Function -
-	 * 
-	 * 
-	 * Returns the number of members with the current selected rank
-	 */
-	function countMembers() {
-		$result = $this->MySQL->query("SELECT * FROM ".$this->MySQL->get_tablePrefix()."members WHERE ".$this->strTableKey." = '".$this->intTableKeyValue."'");
-		$num_rows = $result->num_rows;
-		
-		
-		return $num_rows;
-	}
-	
-	/**
-	 * - getLocalImageURL Function -
-	 * 
-	 * Used to determine if the image attached to the selected rank is a local image or external image.
-	 * 
-	 * Returns FALSE when the image is an external url.
-	 * Returns the local image address when the image is on the server.
-	 * 
-	 */
-	function getLocalImageURL() {
-		global $MAIN_ROOT;
-		$returnVal = false;
-		if($this->intTableKeyValue != "") {
-					
-			if(strpos($this->arrObjInfo['imageurl'], "http://") === false) {
-				
-				$returnVal = $this->arrObjInfo['imageurl'];
-				
 			}
 
+			return $returnVal;
 		}
-		
-		return $returnVal;
-	}
-	
-	/**
-	 * - Refresh Image Size Method -
-	 * 
-	 * If imageheight and imagewidth are not set, this will find the actual size of the image and 
-	 * set it to the arrObjInfo['imagewidth'] and arrObjInfo['imageheight']
-	 * 
-	 */
-	function refreshImageSize() {
-		if($this->intTableKeyValue != "") {
-			if ( $this->arrObjInfo['imageurl'] ) {
-				if($this->arrObjInfo['imagewidth'] == 0) {
-					$imageURL = $this->getLocalImageURL();
-					
-					$imageSize = getimagesize($imageURL);
-					$this->arrObjInfo['imagewidth'] = $imageSize[0];
-				}
-				
-				if($this->arrObjInfo['imageheight'] == 0) {
-					$imageURL = $this->getLocalImageURL();
-				
-					$imageSize = getimagesize($imageURL);
-					$this->arrObjInfo['imageheight'] = $imageSize[1];
+
+		/**
+		 * - Refresh Image Size Method -
+		 *
+		 * If imageheight and imagewidth are not set, this will find the actual size of the image and
+		 * set it to the arrObjInfo['imagewidth'] and arrObjInfo['imageheight']
+		 *
+		 */
+		function refreshImageSize()
+		{
+			if ($this->intTableKeyValue != "") {
+				if ($this->arrObjInfo['imageurl']) {
+					if ($this->arrObjInfo['imagewidth'] == 0) {
+						$imageURL = $this->getLocalImageURL();
+
+						$imageSize = getimagesize($imageURL);
+						if ($imageSize !== false) {
+							$this->arrObjInfo['imagewidth'] = $imageSize[0];
+						}
+					}
+
+					if ($this->arrObjInfo['imageheight'] == 0) {
+						$imageURL = $this->getLocalImageURL();
+
+						$imageSize = getimagesize($imageURL);
+						if ($imageSize !== false) {
+							$this->arrObjInfo['imageheight'] = $imageSize[1];
+						}
+					}
 				}
 			}
 		}
-	}
-	
-	/**
-	 * - delete Method -
-	 * 
-	 * Special delete method for rank to also delete privilege permissions associated with this rank from the rank_privileges table.
-	 */
-	public function delete() {
 
-		$returnVal = false;
-		if($this->intTableKeyValue != "") {
-			
-			
-			$imageURL = $this->getLocalImageURL();
-			if($imageURL !== false) {
-				
-				deleteFile(BASE_DIRECTORY.$imageURL);
-				
+
+/**
+ * - delete Method -
+ *
+ * Special delete method for rank to also delete privilege permissions associated with this rank from the rank_privileges table.
+ */
+		public function delete()
+		{
+
+			$returnVal = false;
+			if ($this->intTableKeyValue != "") {
+
+
+				$imageURL = $this->getLocalImageURL();
+				if ($imageURL !== false) {
+
+					deleteFile(BASE_DIRECTORY . $imageURL);
+
+				}
+
+				$result = $this->MySQL->query("DELETE FROM " . $this->MySQL->get_tablePrefix() . "rank_privileges WHERE " . $this->strTableKey . " = '" . $this->intTableKeyValue . "'");
+				$result = $this->MySQL->query("DELETE FROM " . $this->strTableName . " WHERE " . $this->strTableKey . " = '" . $this->intTableKeyValue . "'");
+				$this->resortOrder();
+
+				if (!$this->MySQL->error) {
+					$returnVal = true;
+				} else {
+					$this->MySQL->displayError("basic.php");
+				}
+
 			}
-			
-			$result = $this->MySQL->query("DELETE FROM ".$this->MySQL->get_tablePrefix()."rank_privileges WHERE ".$this->strTableKey." = '".$this->intTableKeyValue."'");
-			$result = $this->MySQL->query("DELETE FROM ".$this->strTableName." WHERE ".$this->strTableKey." = '".$this->intTableKeyValue."'");
-			$this->resortOrder();
-			
-			if(!$this->MySQL->error) {
-				$returnVal = true;				
-			}
-			else {
-				$this->MySQL->displayError("basic.php");
-			}
-			
+
+			return $returnVal;
+
 		}
-		
-		return $returnVal;
-	
-	}
-	
-	
-	public function get_info($returnSingleValue = "") {
-		global $MAIN_ROOT;
-		$result = parent::get_info($returnSingleValue);
-		
-		if( isset($result['imageurl']) && substr($result['imageurl'],0,4) != "http") {
-			if($returnSingleValue == "") {
-			
-				$fullImageURL = $MAIN_ROOT.$result['imageurl'];
-				$result['imageurl'] = $fullImageURL;
+
+
+		public function get_info($returnSingleValue = "")
+		{
+			global $MAIN_ROOT;
+			$result = parent::get_info($returnSingleValue);
+
+			if (isset($result['imageurl']) && substr($result['imageurl'], 0, 4) != "http") {
+				if ($returnSingleValue == "") {
+
+					$fullImageURL = $MAIN_ROOT . $result['imageurl'];
+					$result['imageurl'] = $fullImageURL;
+				} elseif ($returnSingleValue == "imageurl") {
+					$fullImageURL = $MAIN_ROOT . $result;
+					$result = $fullImageURL;
+				}
 			}
-			elseif($returnSingleValue == "imageurl") {
-				$fullImageURL = $MAIN_ROOT.$result;
-				$result = $fullImageURL;
-			}
+			return $result;
+
 		}
-		return $result;
-		
-	}
-	
-	
-	public function get_info_filtered($returnSingleValue = "") {
-		global $MAIN_ROOT;
-		$result = parent::get_info_filtered($returnSingleValue);
-		
-		if( isset($result['imageurl']) && substr($result['imageurl'],0,4) != "http") {
-			if($returnSingleValue == "") {
-			
-				$fullImageURL = $MAIN_ROOT.$result['imageurl'];
-				$result['imageurl'] = $fullImageURL;
+
+
+		public function get_info_filtered($returnSingleValue = "")
+		{
+			global $MAIN_ROOT;
+			$result = parent::get_info_filtered($returnSingleValue);
+
+			if (isset($result['imageurl']) && substr($result['imageurl'], 0, 4) != "http") {
+				if ($returnSingleValue == "") {
+
+					$fullImageURL = $MAIN_ROOT . $result['imageurl'];
+					$result['imageurl'] = $fullImageURL;
+				} elseif ($returnSingleValue == "imageurl") {
+					$fullImageURL = $MAIN_ROOT . $result;
+					$result = $fullImageURL;
+				}
 			}
-			elseif($returnSingleValue == "imageurl") {
-				$fullImageURL = $MAIN_ROOT.$result;
-				$result = $fullImageURL;
-			}
+
+			return $result;
+
 		}
-		
-		return $result;
-		
+
+
 	}
-	
-	
-}
--- src/themes/include_header.php
+++ src/themes/include_header.php
@@ -1,102 +1,102 @@
 <?php
 
-require_once(BASE_DIRECTORY."include/emailqueue.php");
+	require_once(BASE_DIRECTORY."include/emailqueue.php");
 
-$arrLoginInfo = array();
+	$arrLoginInfo = array();
 
-$taggerObj = new Basic($mysqli, "membersonlypage", "pageurl");
-$siteDomain = $_SERVER['SERVER_NAME'];
+	$taggerObj = new Basic($mysqli, "membersonlypage", "pageurl");
+	$siteDomain = $_SERVER['SERVER_NAME'];
 
-if( ! isset($_SERVER['HTTPS']) || trim($_SERVER['HTTPS']) == "" || $_SERVER['HTTPS'] == "off") {
-	$dispHTTP = "http://";
-}
-else {
-	$dispHTTP = "https://";
-}
+	if( ! isset($_SERVER['HTTPS']) || trim($_SERVER['HTTPS']) == "" || $_SERVER['HTTPS'] == "off") {
+		$dispHTTP = "http://";
+	}
+	else {
+		$dispHTTP = "https://";
+	}
 
 // If user's user/pass cookies are broken, and they had previously selected "Remember Me", fix them
-if(
-	(
-		! isset($_COOKIE['btUsername']) ||
-		! isset($_COOKIE['btPassword'])
-	) &&
-	isset($_SESSION['btRememberMe']) &&
-	$_SESSION['btRememberMe'] == 1 &&
-	isset($_SESSION['btUsername']) &&
-	isset($_SESSION['btPassword'])
-) {
-	setcookie("btUsername", $_SESSION['btUsername'], $COOKIE_EXP_TIME, $MAIN_ROOT);
-	setcookie("btPassword", $_SESSION['btPassword'], $COOKIE_EXP_TIME, $MAIN_ROOT);
-}
+	if(
+		(
+			! isset($_COOKIE['btUsername']) ||
+			! isset($_COOKIE['btPassword'])
+		) &&
+		isset($_SESSION['btRememberMe']) &&
+		$_SESSION['btRememberMe'] == 1 &&
+		isset($_SESSION['btUsername']) &&
+		isset($_SESSION['btPassword'])
+	) {
+		setcookie("btUsername", $_SESSION['btUsername'], $COOKIE_EXP_TIME, $MAIN_ROOT);
+		setcookie("btPassword", $_SESSION['btPassword'], $COOKIE_EXP_TIME, $MAIN_ROOT);
+	}
 
 
-$menuXML = new SimpleXMLElement(BASE_DIRECTORY."themes/".$THEME."/themeinfo.xml", NULL, true);
+	$menuXML = new SimpleXMLElement(BASE_DIRECTORY."themes/".$THEME."/themeinfo.xml", 0, true);
 
 // Check if user is logged in
-if(isset($_SESSION['btUsername']) && isset($_SESSION['btPassword'])) {
+	if(isset($_SESSION['btUsername']) && isset($_SESSION['btPassword'])) {
 
-	$memberObj = new Member($mysqli);
-	if($memberObj->select($_SESSION['btUsername'])) {
+		$memberObj = new Member($mysqli);
+		if($memberObj->select($_SESSION['btUsername'])) {
 
-		if($memberObj->authorizeLogin($_SESSION['btPassword'])) {
-			define("LOGGED_IN", true);
+			if($memberObj->authorizeLogin($_SESSION['btPassword'])) {
+				define("LOGGED_IN", true);
 
-			$memberInfo = $memberObj->get_info();
-			$memberUsername = $memberInfo['username'];
-			$memberID = $memberInfo['member_id'];
+				$memberInfo = $memberObj->get_info();
+				$memberUsername = $memberInfo['username'];
+				$memberID = $memberInfo['member_id'];
 
-			if($memberInfo['loggedin'] == 0) {
-				$memberObj->update(array("loggedin"), array(1));
-			}
-
-
-			$actualPageNameLoc = strrpos($PAGE_NAME," - ");
-			$actualPageName = substr($PAGE_NAME, 0, $actualPageNameLoc);
-
-			if($PAGE_NAME == "") {
-				$actualPageName = "Home Page";
-			}
-
-
-			$lastSeenLink = "<a href='".$dispHTTP.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']."'>".$actualPageName."</a>";
-			$arrUpdateColLastSeen = array("lastseen", "lastseenlink");
-			$arrUpdateValLastSeen = array(time(), $lastSeenLink);
-
-			if((time()-$memberInfo['lastlogin']) > 3600) {
-				$arrUpdateColLastSeen[] = "lastlogin";
-				$arrUpdateValLastSeen[] = time();
-			}
-
-			$memberObj->update($arrUpdateColLastSeen, $arrUpdateValLastSeen);
-
-
-			$rankObj = new Rank($mysqli);
-			$rankObj->select($memberInfo['rank_id']);
-			$rankInfo = $rankObj->get_info();
-			$memberRank = $rankInfo['name'];
-
-			$consoleOptionObj = new ConsoleOption($mysqli);
-
-			
-			// Members Only Tagger			
-			
-			$dispMembersOnlyTagger = "";
-			if(isset($_SESSION['btMembersOnlyTagger']) && $_SESSION['btMembersOnlyTagger'] == 1 && substr($_SERVER['PHP_SELF'], -11) != "console.php") {
-				
-				$pageTaggerURL = $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
-				
-				$taggerCID = $consoleOptionObj->findConsoleIDByName("Member's Only Pages");
-				
-				if($taggerObj->select($pageTaggerURL, false)) {
-					$pageTagStatus = "<span class='pendingFont'>Member's Only</span>";
-					$dispTagOrUntag = "Untag";
+				if($memberInfo['loggedin'] == 0) {
+					$memberObj->update(array("loggedin"), array(1));
 				}
-				else {
-					$pageTagStatus = "<span class='publicNewsColor'>Public</span>";
-					$dispTagOrUntag = "Tag";
+
+
+				$actualPageNameLoc = strrpos($PAGE_NAME," - ");
+				$actualPageName = substr($PAGE_NAME, 0, $actualPageNameLoc);
+
+				if($PAGE_NAME == "") {
+					$actualPageName = "Home Page";
 				}
-				
-				$dispMembersOnlyTagger = "
+
+
+				$lastSeenLink = "<a href='".$dispHTTP.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']."'>".$actualPageName."</a>";
+				$arrUpdateColLastSeen = array("lastseen", "lastseenlink");
+				$arrUpdateValLastSeen = array(time(), $lastSeenLink);
+
+				if((time()-$memberInfo['lastlogin']) > 3600) {
+					$arrUpdateColLastSeen[] = "lastlogin";
+					$arrUpdateValLastSeen[] = time();
+				}
+
+				$memberObj->update($arrUpdateColLastSeen, $arrUpdateValLastSeen);
+
+
+				$rankObj = new Rank($mysqli);
+				$rankObj->select($memberInfo['rank_id']);
+				$rankInfo = $rankObj->get_info();
+				$memberRank = $rankInfo['name'];
+
+				$consoleOptionObj = new ConsoleOption($mysqli);
+
+
+				// Members Only Tagger
+
+				$dispMembersOnlyTagger = "";
+				if(isset($_SESSION['btMembersOnlyTagger']) && $_SESSION['btMembersOnlyTagger'] == 1 && substr($_SERVER['PHP_SELF'], -11) != "console.php") {
+
+					$pageTaggerURL = $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+
+					$taggerCID = $consoleOptionObj->findConsoleIDByName("Member's Only Pages");
+
+					if($taggerObj->select($pageTaggerURL, false)) {
+						$pageTagStatus = "<span class='pendingFont'>Member's Only</span>";
+						$dispTagOrUntag = "Untag";
+					}
+					else {
+						$pageTagStatus = "<span class='publicNewsColor'>Public</span>";
+						$dispTagOrUntag = "Tag";
+					}
+
+					$dispMembersOnlyTagger = "
 				<div id='membersOnlyTagger'>
 				
 				
@@ -149,23 +149,23 @@ if(isset($_SESSION['btUsername']) && isset($_SESSION['btPassword'])) {
 				</script>
 				
 				";
-			}			
-			
+				}
+
+			}
+
 		}
+
 
 	}
 
-
-}
-
-if(!defined("LOGGED_IN")) {
-	define("LOGGED_IN", false);
-}
+	if(!defined("LOGGED_IN")) {
+		define("LOGGED_IN", false);
+	}
 
 
-if($taggerObj->select($_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'], false) && constant('LOGGED_IN') == false) {
+	if($taggerObj->select($_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'], false) && constant('LOGGED_IN') == false) {
 
-	echo "
+		echo "
 	
 		<script type='text/javascript'>
 		
@@ -174,37 +174,37 @@ if($taggerObj->select($_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'], false) &&
 		</script>
 	
 	";
-	
-	exit();
-	
-}
 
+		exit();
 
-$hitCountObj = new Basic($mysqli, "hitcounter", "hit_id");
-$result = $mysqli->query("SELECT * FROM ".$dbprefix."hitcounter WHERE ipaddress = '".$IP_ADDRESS."'");
-if($result->num_rows > 0) {
-	$hitCountRow = $result->fetch_assoc();
-	$hitCountObj->select($hitCountRow['hit_id']);
-	$updateHits = $hitCountObj->get_info("totalhits")+1;
-	
-	
-	$updateColumns = array("totalhits", "pagename");
-	$updateValues = array($updateHits, $PAGE_NAME);
-	
-	if(time() > ($hitCountObj->get_info("dateposted")+1800)) {
-		$updateColumns[] = "dateposted";
-		$updateValues[] = time();	
 	}
-	
-	$hitCountObj->update($updateColumns, $updateValues);
-
-}
-else {
-	$hitCountObj->addNew(array("ipaddress", "dateposted", "pagename", "totalhits"), array($IP_ADDRESS, time(), $PAGE_NAME, 1));
-}
 
 
-$blnDisplayNewsTicker = false;
+	$hitCountObj = new Basic($mysqli, "hitcounter", "hit_id");
+	$result = $mysqli->query("SELECT * FROM ".$dbprefix."hitcounter WHERE ipaddress = '".$IP_ADDRESS."'");
+	if($result->num_rows > 0) {
+		$hitCountRow = $result->fetch_assoc();
+		$hitCountObj->select($hitCountRow['hit_id']);
+		$updateHits = $hitCountObj->get_info("totalhits")+1;
 
 
-$hooksObj->addHook("worldclock-display", "displayDefaultWorldClock");
+		$updateColumns = array("totalhits", "pagename");
+		$updateValues = array($updateHits, $PAGE_NAME);
+
+		if(time() > ($hitCountObj->get_info("dateposted")+1800)) {
+			$updateColumns[] = "dateposted";
+			$updateValues[] = time();
+		}
+
+		$hitCountObj->update($updateColumns, $updateValues);
+
+	}
+	else {
+		$hitCountObj->addNew(array("ipaddress", "dateposted", "pagename", "totalhits"), array($IP_ADDRESS, time(), $PAGE_NAME, 1));
+	}
+
+
+	$blnDisplayNewsTicker = false;
+
+
+	$hooksObj->addHook("worldclock-display", "displayDefaultWorldClock");
