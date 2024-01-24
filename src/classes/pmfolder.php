502f35262f273f42ee35d5416930789a89706768
RedDragonWebDesign <RedDragonWebDesign@gmail.com>
2024-01-23 22:40:46 -0800

    linter: fix whitespace in most recent PR

--- src/classes/pmfolder.php
+++ src/classes/pmfolder.php
@@ -26,7 +26,7 @@
 		}
 
 		public function select($intIDNum, $numericIDOnly = true) {
-			$returnVal = false; 
+			$returnVal = false;
 			$arrSpecialFolders = array("Inbox" => self::INBOX_ID, "Sent Messages" => self::SENTBOX_ID, "Trash" => self::TRASH_ID);
 			if(in_array($intIDNum, $arrSpecialFolders)) {
 				$this->arrObjInfo['name'] = array_search($intIDNum, $arrSpecialFolders);
@@ -34,7 +34,7 @@
 
 			}
 			else {
-				$returnVal = parent::select($intIDNum, $numericIDOnly);	
+				$returnVal = parent::select($intIDNum, $numericIDOnly);
 			}
 
 			return $returnVal;
