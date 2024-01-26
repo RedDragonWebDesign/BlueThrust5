7494e2c63d5c92bfb5ab98e89480d93352be14fc
RedDragonWebDesign <RedDragonWebDesign@gmail.com>
2024-01-26 04:07:44 -0800

    linter: fix a file that is breaking phpcbf

--- src/forum/search.js
+++ src/forum/search.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-	
+
 	$('#btnSearchForum').click(function() {
-		
+
 		var addToURL = "";
-		if($(this).attr('data-topic') != null) {
-			
+		if ($(this).attr('data-topic') != null) {
+			// TODO: add
+		} else if ($(this).attr('data-board') != null) {
+			// TODO: add
 		}
-		else if($(this).attr('data-board') != null) {
-			
-		}
-		
-		window.location = 'search.php?keyword='+$('#
-		
-	});
+
+		// TODO: fix
+		// window.location = 'search.php?keyword='+$('#
 	
+	});
+
 });
