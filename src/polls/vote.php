8cff4f8325d884e56b9015ae367835e1b76d613d
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2024-01-30 14:06:29 -0700

    Fix Issue #52.  Now it will only let you vote multiple times if the setting is enabled.

--- src/polls/vote.php
+++ src/polls/vote.php
@@ -18,10 +18,8 @@
 $prevFolder = "../";
 
 require_once($prevFolder."_setup.php");
-
 require_once($prevFolder."classes/member.php");
 require_once($prevFolder."classes/poll.php");
-
 
 $consoleObj = new ConsoleOption($mysqli);
 $pollObj = new Poll($mysqli);
@@ -29,35 +27,51 @@ $member = new Member($mysqli);
 
 $arrReturn = ["result" => "fail"];
 $pollOptionSelector = "poll_".$_POST['pollID'];
+
 if ($pollObj->select($_POST['pollID'])) {
-	$pollInfo = $pollObj->get_info_filtered();
-	$pollObj->objAccess->arrAccessFor = ["keyName" => "poll_id", "keyValue" => $pollInfo['poll_id']];
+    $pollInfo = $pollObj->get_info_filtered();
+    $pollObj->objAccess->arrAccessFor = ["keyName" => "poll_id", "keyValue" => $pollInfo['poll_id']];
 
-	$blnVote = false;
-	$member->select($_SESSION['btUsername']);
-	$memberID = "";
-	if ($pollInfo['accesstype'] == "members" && $member->authorizeLogin($_SESSION['btPassword'])) {
-		$memberID = $member->get_info("member_id");
-		$blnVote = true;
-	} elseif ($pollInfo['accesstype'] == "memberslimited" && $member->authorizeLogin($_SESSION['btPassword']) && $pollObj->hasAccess($member)) {
-		$memberID = $member->get_info("member_id");
-		$blnVote = true;
-	} elseif ($pollInfo['accesstype'] == "public") {
-		$memberID = ($member->authorizeLogin($_SESSION['btPassword'])) ? $member->get_info("member_id") : "";
-		$blnVote = true;
-	}
+    $blnVote = false;
+    $member->select($_SESSION['btUsername']);
+    $memberID = "";
 
+    if ($pollInfo['accesstype'] == "members" && $member->authorizeLogin($_SESSION['btPassword'])) {
+        $memberID = $member->get_info("member_id");
+        $blnVote = true;
+    } elseif ($pollInfo['accesstype'] == "memberslimited" && $member->authorizeLogin($_SESSION['btPassword']) && $pollObj->hasAccess($member)) {
+        $memberID = $member->get_info("member_id");
+        $blnVote = true;
+    } elseif ($pollInfo['accesstype'] == "public") {
+        $memberID = ($member->authorizeLogin($_SESSION['btPassword'])) ? $member->get_info("member_id") : "";
+        $blnVote = true;
+    }
 
-
-	if ($blnVote) {
-		foreach (json_decode($_POST['pollOptionID'], true) as $pollOptionID) {
-			$pollObj->objPollOption->select($pollOptionID);
-			$pollOptionInfo = $pollObj->objPollOption->get_info_filtered();
-			$arrReturn = $pollObj->vote($memberID, $pollOptionInfo);
-		}
-	} else {
-		$arrReturn['errors'] = "You do not have permission to vote on this poll.";
-	}
+    if ($blnVote) {
+        if ($pollInfo['multivote'] == 1 || !$pollObj->hasVoted($memberID, $_POST['pollID'])) {
+            $voteSuccess = false;
+            foreach (json_decode($_POST['pollOptionID'], true) as $pollOptionID) {
+                $pollObj->objPollOption->select($pollOptionID);
+                $pollOptionInfo = $pollObj->objPollOption->get_info_filtered();
+                $voteResult = $pollObj->vote($memberID, $pollOptionInfo);
+                if ($voteResult['result'] == 'success') {
+                    $voteSuccess = true;
+                }
+            }
+            if ($voteSuccess) {
+                $arrReturn = ["result" => "success", "message" => "Your vote has been recorded."];
+            } else {
+                $arrReturn['errors'] = "There was an error recording your vote.";
+            }
+        } else {
+            $arrReturn['errors'] = "You have already voted in this poll.";
+        }
+    } else {
+        $arrReturn['errors'] = "You do not have permission to vote on this poll.";
+    }
+} else {
+    $arrReturn['errors'] = "Poll not found.";
 }
 
 echo json_encode($arrReturn);
+
