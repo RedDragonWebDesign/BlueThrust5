a0cc8d41faaf27fd5e1a8e517c1065e4ae26a8ab
deepend <deepend@tilde.club>
2024-01-30 17:49:28 -0700

    alphabetize My Account homepage categories.

--- src/members/index.php
+++ src/members/index.php
@@ -65,7 +65,7 @@ if ($checkMember) {
 
 		$strPrivileges = implode(",", $rankPrivileges);
 
-		$result = $mysqli->query("SELECT * FROM ".$mysqli->get_tablePrefix()."consolecategory ORDER BY ordernum DESC");
+		$result = $mysqli->query("SELECT * FROM ".$mysqli->get_tablePrefix()."consolecategory ORDER BY name ASC");
 
 		while ($row = $result->fetch_assoc()) {
 			$arrConsoleCats[] = $row['consolecategory_id'];
